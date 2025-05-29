c8a83f49cd2cf2c1cfad0e25b73738d9e0c2e3a1
1111 <40191444+wudidejimo@users.noreply.github.com>
2025-05-30 00:24:20 +0800

    Update socketA.py

--- Mycode/Ours/socketA.py
+++ Mycode/Ours/socketA.py
@@ -8,17 +8,17 @@ from FunctionMod import *
 
 
 
-# socke配置
+
 NodeA = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = ('127.0.0.1', 123)
 NodeA.connect(server_address)
 
-# 获取注册信息
+
 client_type = "A"
 
 deltT = 1.0
 
-# 初始化阶段 注册阶段
+
 IDi = secrets.token_bytes(16).hex()
 PWi = secrets.token_bytes(16).hex()
 a = secrets.token_bytes(32).hex()
@@ -39,7 +39,7 @@ ki_star =  xor_strings(ki,xor_strings(RPWi,Bio))
 V0 = hash_256(IDi, RPWi,ki,hash_mk)
 
 
-# 等待Server的通知 收到通知就开始计时
+
 IDj = pickle.loads(NodeA.recv(1024))
 
 # 认证开始========================================================================================
@@ -51,7 +51,7 @@ ki1 = xor_strings(ki_star, xor_strings(RPWi_star, Bio))
 V0_star = hash_256(IDi, RPWi_star, ki1,hash_mk_star)
 
 if V0_star != V0:
-    raise ValueError("Ai验证失败")
+    raise ValueError("Aifail")
 ki1 = ki1[:32]
 hash_mk_star1 = hash_mk_star[:32]
 r1 = secrets.token_bytes(16).hex()
@@ -66,18 +66,18 @@ section_A1 = time.perf_counter() - t1
 
 M1 = [X1,X2, V1, T1]
 NodeA.send(pickle.dumps(M1))
-# M1发送成功
+
 
 
 t2 = time.perf_counter() 
 NodeA_sectionOne_T1 = (t2-t1)*1000
 
-# 接收消息4
+
 X6, V4, T4= pickle.loads(NodeA.recv(1024))
 t3 = time.perf_counter() 
 
 if time.time()-T4>deltT:
-    raise ValueError("T3过期")
+    raise ValueError("T3out")
 
 temp = xor_strings(X6, hash_256(TIDi, S1))
 SK_star = temp[32:]
@@ -86,7 +86,7 @@ TIDi_new = temp[:32]
 V4_star = hash_256(X6,  S1, SK_star,TIDi_new, T4)
 
 if V4_star !=V4:
-    raise ValueError("MSG4验证失败")
+    raise ValueError("MSG4fail")
 
 
 section_A2 = (time.perf_counter() -t3)*1000
@@ -95,7 +95,7 @@ section_A2 = (time.perf_counter() -t3)*1000
 print("NodeA_section:",section_A1+section_A2)
 
 t4 = time.perf_counter()
-print("总时间: ", (t4 - t1) * 1000)
+print("Time: ", (t4 - t1) * 1000)
 
-# 关闭连接
+
 NodeA.close()
